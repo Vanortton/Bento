3e59f0801c76865ece5e97603113abea6398b808
Vanortton <vanortton@gmail.com>
2022-10-07 09:22:20 -0300

    🍱 Redesigning the file layout

---
+++ assets/js/settings.js
@@ -0,0 +1,57 @@
+function imageUploaded() {
+    var file = document.querySelector('input#uploadImageBackground')['files'][0]
+    var reader = new FileReader()
+    reader.onload = function () {
+        base64String = reader.result
+        imageBase64Stringsep = base64String
+    }
+    reader.readAsDataURL(file)
+}
+
+function saveData() {
+    const inputs = {
+        name: document.querySelector('input[name="name"]'),
+        imageBackground: document.querySelector('input[name="imageBackground"]:checked'),
+        openInNewTab: document.querySelector('input[name="openInNewTab"]:checked'),
+        twelveHourFormat: document.querySelector('input[name="twelveHourFormat"]:checked'),
+        bentoLayout: document.querySelector('input[name="bentoLayout"]:checked'),
+        weatherIcons: document.querySelector('input[name="weatherIcons"]:checked'),
+        changeThemeByHour: document.querySelector('input[name="changeThemeByHour"]:checked'),
+        hourDarkThemeActive: document.querySelector('input[name="hourDarkThemeActive"]'),
+        hourDarkThemeInactive: document.querySelector('input[name="hourDarkThemeInactive"]'),
+        changeThemeByOS: document.querySelector('input[name="changeThemeByOS"]:checked'),
+        autoChangeTheme: document.querySelector('input[name="autoChangeTheme"]:checked'),
+    }
+
+    for (let input in inputs) {
+        const toTrueOrFalse = value => value === 'true' ? true : value === 'false' ? false : value
+        inputs[input] != null ? CONFIGSaved[`${inputs[input].name}`] = toTrueOrFalse(inputs[input].value) : ''
+        window.localStorage.setItem('CONFIG', JSON.stringify(CONFIGSaved))
+        objectSaved = JSON.parse(window.localStorage.getItem('CONFIG'))
+        CONFIGSaved = { ...objectSaved }
+    }
+
+    CONFIGSaved.dataImage = base64String
+    window.localStorage.setItem('CONFIG', JSON.stringify(CONFIGSaved))
+
+    document.querySelector('.alert').style.transform = "scaleX(1) translate(-50%, -50%)"
+    document.querySelector('.alert').style.opacity = "1"
+    document.querySelector('.alert').innerHTML = "Alterações salvas"
+    setTimeout(() => {
+        document.querySelector('.alert').style.transform = "scaleX(0) translate(-50%, -50%)"
+        document.querySelector('.alert').style.opacity = "0"
+        window.location.reload()
+    }, 1500)
+}
+
+document.querySelector('button[btn-salvar]').onclick = (e) => {
+    e.preventDefault()
+    saveData()
+}
+document.querySelector('input[name="name"]').value = CONFIGSaved.name ? CONFIGSaved.name : 'User'
+
+for (const sett in CONFIGSaved) {
+    sett !== 'name' && sett !== 'hourDarkThemeActive' && sett !== 'hourDarkThemeInactive' && sett !== 'dataImage' ?
+        document.querySelector(`input[name="${sett}"][value="${CONFIGSaved[sett]}"]`).checked = true : ''
+    sett === 'hourDarkThemeActive' || sett === 'hourDarkThemeInactive' ? document.querySelector(`input[name="${sett}"]`).value = CONFIGSaved[sett] : ''
+}
--- config.js
+++ config.js
@@ -211,68 +211,12 @@ let PRECONFIG = {
 	autoChangeTheme: true
 }
 
-let buttons = {
-	firstButtonsContainer: { ...CONFIG.firstButtonsContainer },
-	secondButtonsContainer: { ...CONFIG.secondButtonsContainer }
-}
-
-// let lists = { 
-// 	firstListsContainer: {...CONFIG.firstListsContainer},
-// 	secondListsContainer: { ...CONFIG.secondListsContainer }
-// }
-
 JSON.parse(window.localStorage.getItem('CONFIG')) == null ?
 	window.localStorage.setItem('CONFIG', JSON.stringify(PRECONFIG)) : PRECONFIG = JSON.parse(window.localStorage.getItem('CONFIG'))
 let objectSaved = JSON.parse(window.localStorage.getItem('CONFIG'))
 
 let CONFIGSaved = { ...objectSaved }
 let base64String = objectSaved.dataImage || "assets/background.jpg"
-
-function imageUploaded() {
-	var file = document.querySelector('input#uploadImageBackground')['files'][0]
-	var reader = new FileReader()
-	reader.onload = function () {
-		base64String = reader.result
-		imageBase64Stringsep = base64String
-	}
-	reader.readAsDataURL(file)
-}
-
-function saveData() {
-	const inputs = {
-		name: document.querySelector('input[name="name"]'),
-		imageBackground: document.querySelector('input[name="imageBackground"]:checked'),
-		openInNewTab: document.querySelector('input[name="openInNewTab"]:checked'),
-		twelveHourFormat: document.querySelector('input[name="twelveHourFormat"]:checked'),
-		bentoLayout: document.querySelector('input[name="bentoLayout"]:checked'),
-		weatherIcons: document.querySelector('input[name="weatherIcons"]:checked'),
-		changeThemeByHour: document.querySelector('input[name="changeThemeByHour"]:checked'),
-		hourDarkThemeActive: document.querySelector('input[name="hourDarkThemeActive"]'),
-		hourDarkThemeInactive: document.querySelector('input[name="hourDarkThemeInactive"]'),
-		changeThemeByOS: document.querySelector('input[name="changeThemeByOS"]:checked'),
-		autoChangeTheme: document.querySelector('input[name="autoChangeTheme"]:checked'),
-	}
-
-	for (let input in inputs) {
-		const toTrueOrFalse = value => value === 'true' ? true : value === 'false' ? false : value
-		inputs[input] != null ? CONFIGSaved[`${inputs[input].name}`] = toTrueOrFalse(inputs[input].value) : ''
-		window.localStorage.setItem('CONFIG', JSON.stringify(CONFIGSaved))
-		objectSaved = JSON.parse(window.localStorage.getItem('CONFIG'))
-		CONFIGSaved = { ...objectSaved }
-	}
-
-	CONFIGSaved.dataImage = base64String
-	window.localStorage.setItem('CONFIG', JSON.stringify(CONFIGSaved))
-
-	document.querySelector('.alert').style.transform = "scaleX(1) translate(-50%, -50%)"
-	document.querySelector('.alert').style.opacity = "1"
-	document.querySelector('.alert').innerHTML = "Alterações salvas"
-	setTimeout(() => {
-		document.querySelector('.alert').style.transform = "scaleX(0) translate(-50%, -50%)"
-		document.querySelector('.alert').style.opacity = "0"
-		window.location.reload()
-	}, 1500)
-}
 
 for (let sett in CONFIGSaved) {
 	CONFIG[`${sett}`] = CONFIGSaved[sett]
